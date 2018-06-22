3a91ae7646fe7a5bdd03e1a58c9235d4646acc10
lesleyfon <lesleybanadzem2017@gmail.com>
2018-06-21 22:15:42 -0700

    added some styling to the page

--- public/app.js
+++ public/app.js
@@ -3,10 +3,16 @@ $(document).ready(function () {
         $.get("/scrape", function (data) {
             alert("Scraped 20 new Articles")
             for (var i = 0; i < data.length; i++) {
-                $("#articles").append(`<div id="artDiv"><a id="artLink" href="${data[i].link}" target="_blank"><h1 id="artTitle">${data[i].title}</h1></a></div>`)
-
-
-            }
+                $("#articles").append(`
+                <div id="artDiv">
+                    <a id="artLink" href="${data[i].link}" target="_blank">
+                        <h1 id="artTitle">${data[i].title}</h1>
+                    </a>
+                </div>`
+            )}
         })
+    })
+    $("#home").on("click", function(){
+        $.get("/",)
     })
 })
